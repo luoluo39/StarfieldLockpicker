c6cb2ec9ce197580000da0b5a9573a7658926d17
luoluo39 <tremendoushard@outlook.com>
2023-09-09 20:56:18 +0800

    remove unused codes

--- StarfieldLockpicker/Utility.cs
+++ StarfieldLockpicker/Utility.cs
@@ -51,48 +51,13 @@ public static class Utility
         return new Vector2(x, y);
     }
 
-    public static float ScaleRadius(float value)
+    public static float ScaleRadius(float value, AppConfig? config = null)
     {
-        var config = AppConfig.Instance;
+        config ??= AppConfig.Instance;
+
         return value * config.ScreenUIScale / config.ReferenceUIScale;
     }
-
-    public static float TranslatePositionX(float value)
-    {
-        var config = AppConfig.Instance;
-        var rx = (value * 2 - config.ReferenceResolutionWidth) / config.ReferenceUIWidth;
-        var x = (rx * config.ScreenUIWidth + config.ScreenWidth) / 2;
-        return x;
-    }
-
-    public static float TranslatePositionY(float value)
-    {
-        var config = AppConfig.Instance;
-        var ry = (value * 2 - config.ReferenceResolutionHeight) / config.ReferenceUIHeight;
-        var y = (ry * config.ScreenUIHeight + config.ScreenHeight) / 2;
-        return y;
-    }
-
-    public static Bitmap CaptureScreen(int display)
-    {
-        var captureRectangle = Screen.AllScreens[display].Bounds;
-        var captureBitmap = new Bitmap(captureRectangle.Size.Width, captureRectangle.Size.Height, PixelFormat.Format32bppArgb);
-        using var captureGraphics = Graphics.FromImage(captureBitmap);
-        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-        return captureBitmap;
-    }
-
-    public static void CaptureScreen(Bitmap bitmap, int display)
-    {
-        var captureRectangle = Screen.AllScreens[display].Bounds;
-
-        if (bitmap.Size != captureRectangle.Size)
-            throw new ArgumentException(nameof(bitmap));
-
-        using var captureGraphics = Graphics.FromImage(bitmap);
-        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-    }
-
+    
     public static void CaptureScreenArea(Bitmap bitmap, int display, Rectangle rect)
     {
         if (bitmap.Size != rect.Size)
@@ -145,50 +110,6 @@ public static class Utility
         bmp2.Bitmap.UnlockBits(data2);
 
         double msePerPixel = mse / (rectangle.Width * rectangle.Height * 3.0);
-        return msePerPixel;
-    }
-
-    public static double CalculateKeyAreaMSE(Bitmap bmp1, Bitmap bmp2)
-    {
-        if (bmp1.Size != bmp2.Size)
-            throw new ArgumentException("Bitmaps must have the same dimensions.");
-
-        int width = bmp1.Width;
-        int height = bmp1.Height;
-        double mse = 0;
-
-        BitmapData data1 = bmp1.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-        BitmapData data2 = bmp2.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
-
-        //1333,130,494,744 on 1080p
-
-        var config = AppConfig.Instance;
-
-        var x0 = (int)TranslatePositionX(config.KeyAreaX0);
-        var y0 = (int)TranslatePositionY(config.KeyAreaY0);
-
-        var x1 = (int)TranslatePositionX(config.KeyAreaX0 + config.KeyAreaWidth);
-        var y1 = (int)TranslatePositionY(config.KeyAreaY0 + config.KeyAreaHeight);
-
-        unsafe
-        {
-            for (int y = y0; y < y1; y++)
-            {
-                for (int x = x0; x < x1; x++)
-                {
-                    var color1 = Color.FromArgb(*(int*)(data1.Scan0 + (data1.Stride * y + x * 4)));
-                    var color2 = Color.FromArgb(*(int*)(data2.Scan0 + (data2.Stride * y + x * 4)));
-                    var cv1 = new Vector3(color1.R, color1.G, color1.B);
-                    var cv2 = new Vector3(color2.R, color2.G, color2.B);
-                    mse += Vector3.DistanceSquared(cv1, cv2);
-                }
-            }
-        }
-
-        bmp1.UnlockBits(data1);
-        bmp2.UnlockBits(data2);
-
-        double msePerPixel = mse / ((x1 - x0) * (y1 - y0) * 3.0);
         return msePerPixel;
     }
 
